e24dcddd4f35121040f4bd671c51c2fff9a2836d
Gopal Pokhrel <pokhrelgopal27@gmail.com>
2024-03-02 13:07:11 +0545

    added light and dark mode in frontend

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+  if (!mounted) <>{children}</>;
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+}
