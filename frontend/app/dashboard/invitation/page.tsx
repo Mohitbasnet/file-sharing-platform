bbf1661ccf71f153f8bf6674fb79b239529ad845
Gopal Pokhrel <pokhrelgopal27@gmail.com>
2024-03-10 23:15:36 +0545

    changed interface for user invite and fixed some bugs related to TS

--- frontend/app/dashboard/invitation/page.tsx
+++ frontend/app/dashboard/invitation/page.tsx
@@ -3,9 +3,23 @@ import React from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { apiGetInvitations } from "@/lib/apiRequests";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "@/components/elements/Spinner";
 
 const Account = () => {
   const handleSubmit = () => {};
+  const {
+    isLoading,
+    error,
+    data: invitations,
+  } = useQuery<any>({
+    queryKey: ["invitations"],
+    queryFn: () => apiGetInvitations(),
+  });
+
+  if (isLoading) return <Spinner />;
+  console.log(invitations);
   return (
     <main>
       <h1 className="text-2xl font-bold">Your Invitations</h1>
